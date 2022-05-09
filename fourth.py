15a57a9376dbb0482c7305aa7dd2f692a125de90
Wallace Custódio Batista <wallacecbatista@gmail.com>
2022-05-09 18:51:30 -0300

    Fourth class
    We learned a little about list, tuple and how to manipulate the list with basic functions

---
+++ fourth.py
@@ -0,0 +1,64 @@
+lista = [12, 3, 15, 27]
+lista_animais = ['Cachorro', 'Gato', 'Elefante', 'Lobo', 'Arara']
+#print(lista_animais[0])
+#soma = 0
+# for x in lista:
+#     print(x)
+#     soma += x
+# print(soma)
+
+#print(sum(lista))
+#Para Somar os Valores da lista
+#print(max(lista))
+#Para Mostrar o maior valor da lista
+#print(min(lista))
+#Para Mostrar o menor Valor da lista
+
+# nova_lista = lista_animais * 3
+# print(nova_lista)
+
+
+# if 'Lobo' in lista_animais:
+#     print('Existe Lobo na Lista')
+# else:
+#     print('Não Existe Lobo na lista')
+#     lista_animais.append('Lobo')
+#     print(lista_animais)
+
+# lista_animais.pop(2)
+#lista_animais.remove('Elefante')
+# lista.sort()
+# lista_animais.sort()
+# print(lista)
+# print(lista_animais)
+# lista.reverse()
+# lista_animais.reverse()
+# print(lista)
+# print(lista_animais)
+
+# lista_animais [0] = 'Macaco'
+# print(lista_animais)
+#Para Alterar o valor pelo numero que esta
+
+#print(len(lista_animais))
+
+# tupla_animais = tuple(lista_animais)
+# print(type(tupla_animais))
+# print(tupla_animais)
+# lista_animal = list(tupla_animais)
+# print(type(lista_animal))
+# print(lista_animal)
+
+# tupla = (1, 10, 12, 14)
+# print(tupla)
+#Não Pode se alterar o valor da tupla!!
+
+#.append() Função Para inserir dados na lista
+#.remove() Para passar o nome do parametro a ser removido
+#.pop() Função para retirar o ultimo elemento da lista, ou um determinado elemento
+#Se colocar o numero onde ele estiver
+#.sort() Para ordenar a Lista
+#.reverse() Para reverter a ordem da Lista
+#len() Para saber quantos elementos tem na Lista ou Tupla
+#tuple() Para trasnformar uma lista em Tupla
+#list() Para transformar uma tuple em lista
